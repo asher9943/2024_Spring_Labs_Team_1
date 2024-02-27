dcddc25e87648032616d1ae69677f0773a955dd1
Asher Anand <asherganand@gmail.com>
2024-02-27 13:15:57 -0500

    Part C, D works, tested on xavier

--- Tutorial_5/part_c_display.py
+++ Tutorial_5/part_c_display.py
@@ -6,6 +6,11 @@ from scipy.io import wavfile
 sample_rate, samples = wavfile.read('file.wav')
 frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
 
+plt.plot(np.arange(len(samples)), samples)
+plt.ylabel('Amplitude')
+plt.xlabel('Sample')
+plt.show()
+
 plt.pcolormesh(times, frequencies, np.log(spectrogram))
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
