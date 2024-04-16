a369ac6edcb8e23643d5f51eb690f2d239ea5e74
Stephen Callis <smcallis9@gmail.com>
2024-04-16 13:20:35 -0400

    semi-working asteroid field backup - needs testing

---
+++ Project_Blue/src/asteroidfield.hpp
@@ -0,0 +1,14 @@
+#ifndef ASTEROID_H_
+#define ASTEROID_H_
+
+#include <Adafruit_MCP3008.h>
+#include <Adafruit_MPU6050.h>
+#include <Encoder.h>
+
+#ifndef MOVEMENT_H_
+#include "movement.hpp"
+#endif
+
+void move_around();
+
+#endif
