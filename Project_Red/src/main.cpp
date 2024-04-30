7f090c97236370dea5b82c4b99a3ba6d90a39e6c
Donald Buckshaw <dbucksha@terpmail.umd.edu>
2024-04-30 09:28:33 -0400

    update wuth wifi markers

--- Project_Red/src/main.cpp
+++ Project_Red/src/main.cpp
@@ -28,7 +28,9 @@ void loop() {
  
   delay(2000);
 
-  if(state == 0) { // Line of the Republic
+  // TODO wait for go from computer
+
+  if(state == 0) { // Line of the Republic ---------------------------
     transition = 0;
 
     do {
@@ -38,9 +40,10 @@ void loop() {
     
     transition_right();
     state = 1;
+    // tell next robot to go?
 
 
-  } else if(state == 1) { // Maze of Mandalore
+  } else if(state == 1) { // Maze of Mandalore ----------------------
     transition = 0;
 
     // find maze entrance
@@ -49,19 +52,19 @@ void loop() {
       delay(50);
     } while(lineArray[12] == 0);
     
+    // skip maze
     turnAngle(-90);
 
-    // skip maze
     int count = 0;
     do {
       updateMoveForwardPID(1);
       delay(50);
       count++;
-    } while(count <= 60); //test
+    } while(count <= 60); //TODO test
 
     turnAngle(90);
 
-    // find line
+    // find line again
     do {
       moveForwardDist(enc1, 10);
       readLineSensor();
@@ -69,6 +72,7 @@ void loop() {
     
     turnCorner(enc1, 0);
 
+    // finish section
     do {
       updateLineFollow(enc1);
       delay(50);
@@ -78,11 +82,11 @@ void loop() {
     state = 2;
 
 
-  } else if(state == 2) { // Kessel Run
+  } else if(state == 2) { // Kessel Run -----------------------------
     transition = 0;
 
     do {
-      updateLineFollow(enc1);
+      updateLineFollow(enc1); //TODO wifi check
       delay(50);
     } while(transition == 0);
     
@@ -90,7 +94,7 @@ void loop() {
     state = 3;
 
 
-  } else if(state == 3) { // Hoth Asteroid Field
+  } else if(state == 3) { // Hoth Asteroid Field ---------------------
     transition = 0;
 
     // get parallel to line
@@ -101,23 +105,24 @@ void loop() {
     // skip asteroid field
     int count = 0;
     do {
-      updateMoveForwardPID(1);
+      updateMoveForwardPID(1); //TODO wifi check
       delay(50);
       count++;
-    } while(count <= 100); //test
+    } while(count <= 100); //TODO test
     
+    // find line again
     turnAngle(-90);
-
-    // find line
+    
     do {
-      moveForwardDist(enc1, 10);
+      moveForwardDist(enc1, 10); //TODO wifi check
       readLineSensor();
     } while(lineArray[6] != 1);
     
-    turnAngle(90);
+    turnCorner(enc1, 1);
 
+    // finish section
     do {
-      updateLineFollow(enc1);
+      updateLineFollow(enc1); //TODO wifi check
       delay(50);
     } while(transition == 0);
 
@@ -125,22 +130,57 @@ void loop() {
     state = 4;
 
 
-  } else if(state == 4) { // Path of Dual Fates
+  } else if(state == 4) { // Path720 of Dual Fates ---------------------
     transition = 0;
 
     do {
-      updateLineFollow(enc1);
+      updateLineFollow(enc1); //TODO wifi check
+      delay(50);
+    } while(!(lineArray[0] == 1 && lineArray[12] == 1));
+    
+    // tell computer to listen
+
+
+    while(0); //wait for information
+
+    // select direction
+    if(1) {   // left
+      turnCorner(enc1, 1);
+
+      do {
+        updateLineFollow(enc1); //TODO wifi check
+        delay(50);
+      } while(lineArray[12] == 0);
+
+      turnCorner(enc1, 1);
+
+    } else {  // right
+      turnCorner(enc1, 0);
+
+      do {
+        updateLineFollow(enc1); //TODO wifi check
+        delay(50);
+      } while(lineArray[0] == 0);
+
+      turnCorner(enc1, 0);
+
+    }
+    
+    // follow line to end
+    do {
+      updateLineFollow(enc1); //TODO wifi check
       delay(50);
     } while(transition == 0);
-    
+
     transition_left();
     state = 5;
 
 
-  } else if(state == 5) { // Second Line of the Republic
+  } else if(state == 5) { // Second Line of the Republic ------------
     transition = 0;
+
     do {
-      updateLineFollow(enc1);
+      updateLineFollow(enc1); //TODO wifi check
       delay(50);
     } while(transition == 0);
     
@@ -148,25 +188,31 @@ void loop() {
     state = 6;
 
 
-  } else if(state == 6) { // Endor Dash
+  } else if(state == 6) { // Endor Dash ----------------------------
     transition = 0;
 
     do {
-      updateLineFollow(enc1);
+      updateLineFollow(enc1); //TODO wifi check
       delay(50);
       readLineSensor();
     } while(lineArray[6] == 1);
     
     do {
-      updateMoveForwardPID(1);
+      updateMoveForwardPID(1); //TODO wifi check
       delay(50);
       readLineSensor();
-    } while(lineArray[6] == 0); //test
+    } while(lineArray[6] == 0);
 
     moveForwardDist(enc1, 50);
 
     state = 7;
-  } else { // Finish
 
+
+  } else { // Finish -----------------------------------------------
+    while(1) {
+      turnAngle(45);
+     //TODO wifi check
+    }
+    
   }  
 }
